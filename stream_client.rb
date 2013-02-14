f479394a79819c87a5e0fe78fd17a8c2ad03ad49
Perry Huang <huang185@illinois.edu>
2013-02-14 16:59:54 -0600

    update

--- stream_client.rb
+++ stream_client.rb
@@ -3,9 +3,11 @@ require 'socket'
 host, port = ARGV
 
 s = TCPSocket.open(host, port)
+start_time = Time.now
 
 while line = s.gets
-	puts line.size
+	#puts line.size
+	puts line.size.to_s + " bytes received at " + ((Time.now - start_time) * 1000).to_s
 end
 
 s.close
