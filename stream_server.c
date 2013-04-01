59608844e2be279420bb482042b5f1c12fb154fd
Perry Huang <huang185@illinois.edu>
2013-04-01 00:40:22 -0500

    current_minus_initial from unsigned long to double.

--- stream_server.c
+++ stream_server.c
@@ -61,20 +61,23 @@ int main(int argc, char *argv[]) {
     char output_buffer[100000];
     struct timespec initial_time, current_time;
     int new_fd;
-    int recv_len;
+    int recv_len = 1;
+    double current_minus_initial = 0;
 
     setpriority(PRIO_PROCESS, 0, -20);
     socket_fd = initialize_connection();
 
     addr_size = sizeof their_addr;
 
-    clock_gettime(CLOCK_REALTIME, &initial_time);
     new_fd = accept(socket_fd, (struct sockaddr *)&their_addr, &addr_size);
+    clock_gettime(CLOCK_REALTIME, &initial_time);
 
-
-    while(1) {
+    while(recv_len != 0) {
     	recv_len = recv(new_fd, &output_buffer, 100000, 0);
-	    printf("%d\n", recv_len);
+	    clock_gettime(CLOCK_REALTIME, &current_time);
+	    current_minus_initial = (current_time.tv_sec - initial_time.tv_sec) * 1000.0;
+	   	current_minus_initial += (current_time.tv_nsec - initial_time.tv_nsec) / 1000000.0;
+	    printf("%d at %f\n", recv_len, current_minus_initial);
 	}
 
     close(socket_fd);
