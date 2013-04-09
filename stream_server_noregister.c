fae899699ec519ded63fff69f1e8cf4bfb2e32cd
Perry Huang <huang185@illinois.edu>
2013-04-09 00:20:55 -0500

    Wait for entire message to arrive.

--- stream_server_noregister.c
+++ stream_server_noregister.c
@@ -14,6 +14,7 @@
 #include <netinet/in.h>
 
 #define PORT "2000"
+#define MSG_SIZES_FILENAME "sizes.csv"
 
 int initialize_connection() {
     int socket_fd;
@@ -58,28 +59,40 @@ int main(int argc, char *argv[]) {
     int socket_fd;
     int time_stamp;
     int num_bytes = 1;
-    char output_buffer[100000];
+    char output_buffer[1000000];
     struct timespec initial_time, current_time;
     int new_fd;
     int recv_len = 1;
     double current_minus_initial = 0;
+    FILE *input_csv = NULL;
+    int frame_size = 0;
 
     setpriority(PRIO_PROCESS, 0, -20);
     socket_fd = initialize_connection();
+    input_csv = fopen(MSG_SIZES_FILENAME, "r");
 
+    if(input_csv == NULL) {
+        perror("fopen error");
+        exit(1);
+    }
+
+    
     addr_size = sizeof their_addr;
 
     new_fd = accept(socket_fd, (struct sockaddr *)&their_addr, &addr_size);
     clock_gettime(CLOCK_REALTIME, &initial_time);
 
-    while(recv_len != 0) {
-    	recv_len = recv(new_fd, &output_buffer, 100000, 0);
+//    while(recv_len != 0) {
+        while(fscanf(input_csv, "%d\n", &frame_size) != EOF) {
+    	recv_len = recv(new_fd, &output_buffer, frame_size, MSG_WAITALL);
 	    clock_gettime(CLOCK_REALTIME, &current_time);
 	    current_minus_initial = (current_time.tv_sec - initial_time.tv_sec) * 1000.0;
 	   	current_minus_initial += (current_time.tv_nsec - initial_time.tv_nsec) / 1000000.0;
-	    printf("%d at %f\n", recv_len, current_minus_initial);
+	    printf("%d,%f\n", recv_len, current_minus_initial);
+//            printf("%d\n", recv_len);
 	}
 
     close(socket_fd);
     return 0;
 }
+
