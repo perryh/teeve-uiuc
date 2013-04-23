6cc62867788177903ddcb8c06576d984a29c22a9
teeve <teeve@teeve-Precision-WorkStation-690.(none)>
2013-04-23 01:32:39 -0500

    Move files.

--- old_files/stream_server.rb
+++ old_files/stream_server.rb
@@ -1,22 +1,16 @@
+#!/home/perryh/.rvm/bin/ruby
+
 require 'socket'
-require 'csv'
 
-output_string = "x" * 100000
+#Process.setpriority(Process::PRIO_PROCESS, 0, -20)
+server = TCPServer.new 2000 # Server bind to port 2000
 
-input_name = ARGV[0]
-input_file = File.open(input_name, "r")
-server = TCPServer.open(2000)
-input_csv = CSV.parse(input_file, {:headers => true})
-
-loop {
-  Thread.start(server.accept) do |client|
-    start_time = Time.now
-    input_csv.each do |row|
-      frame_size = row['FrameSize'].to_i
-      while(((Time.now - start_time) * 1000) < row['Timestamp'].to_i)
-      end
-      client.write(output_string[0, frame_size] + "\n")
-    end
-    client.close
-    end
-}
+loop do
+  client = server.accept    # Wait for a client to connect
+  start_time = Time.now
+  while input = client.recv(1000000)
+  	current_time = Time.now
+    puts input.length.to_s + " at " + ((current_time - start_time) * 1000).to_s
+  end
+  client.close
+end
